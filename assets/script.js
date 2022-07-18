1968535f280e486dd585787afc819f9003d29928
Vinicius <abreu-vinicius15@hotmail.com>
2022-07-18 17:02:06 -0300

    Projeto contador finalizado, e finalizado os comentarios.

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,33 @@
 var Numero_Atual_Wrapper = document.getElementById("Numero_Atual");
 var Numero_Atual = 0;
 
-function increment(){   
+function increment() {
 
-Numero_Atual = Numero_Atual +1;
-Numero_Atual_Wrapper.innerHTML = Numero_Atual;
+    //Icrementa mais um no valor ao clicar no botão de soma.
+    Numero_Atual = Numero_Atual + 1;
+    //Passa o valor na tela através do InnerHtml
+    Numero_Atual_Wrapper.innerHTML = Numero_Atual;
 
-console.log(Numero_Atual);
-if (Numero_Atual > 9) {
-    
-    document.getElementById("soma").disabled = true;
-    alert("Não podemos passar de 10!");
-}   
+    //console.log(Numero_Atual);
 
+    //Valida se o número exibido na tela é maior que 10, e se verdadeiro ele desabilita o botão de soma.
+    if (Numero_Atual > 9) {
+
+        document.getElementById("soma").disabled = true;
+        alert("Não podemos passar de 10!");
     }
 
-function decrement(){
+}
+
+function decrement() {
     Numero_Atual = Numero_Atual - 1;
     Numero_Atual_Wrapper.innerHTML = Numero_Atual;
 
-    if (Numero_Atual <= 9){
+    //Valida se o número exibido na tela é menor que 10, e se verdadeiro ele reabilita o botão de soma.
+    if (Numero_Atual <= 9) {
 
         document.getElementById("soma").disabled = false;
-     }
+    }
     //if (Numero_Atual < 0) {console.log("Negativo");}
 }
 
